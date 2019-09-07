254c2a6cf6843db7bda40c79f9dc6e369fc1a35c
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-07 23:05:03 +0200

    Updates templates and demo data (patch)

--- api/document-preview.js
+++ api/document-preview.js
@@ -4,6 +4,11 @@ const generateTemplate = require('../lib/generate-template')
 module.exports = async (request, response) => {
   const payload = await request.body
   const template = generateTemplate(payload)
-  const document = axios.post(process.env.PDF_SERVICE_URL, template)
-  response.send(document)
+  try {
+    const document = await axios.post(process.env.PDF_SERVICE_URL, template)
+    response.send(document)
+  } catch (error) {
+    console.error(error)
+    response.send(error)
+  }
 }
