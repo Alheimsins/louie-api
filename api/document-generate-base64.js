3f93f1b6c60c0605f5f203ef74915eec3e54673c
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-08 12:38:54 +0200

    Quick return for OPTIONS (patch)

--- api/document-generate-base64.js
+++ api/document-generate-base64.js
@@ -2,8 +2,12 @@ const fetch = require('node-fetch')
 const generateTemplate = require('../lib/generate-template')
 
 module.exports = async (request, response) => {
-  const payload = await request.body
-  const template = generateTemplate(payload)
-  const data = await fetch(process.env.PDF_SERVICE_URL, { method: 'POST', body: JSON.stringify(template), headers: { 'Content-Type': 'application/json' } }).then(response => response.buffer())
-  response.send(`data:application/pdf;base64,${data.toString('base64')}`)
+  if (request.method.toLowerCase() === 'options') {
+    response.send('')
+  } else {
+    const payload = await request.body
+    const template = generateTemplate(payload)
+    const data = await fetch(process.env.PDF_SERVICE_URL, { method: 'POST', body: JSON.stringify(template), headers: { 'Content-Type': 'application/json' } }).then(response => response.buffer())
+    response.send(`data:application/pdf;base64,${data.toString('base64')}`)
+  }
 }
