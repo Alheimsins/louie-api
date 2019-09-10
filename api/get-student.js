9636a9c50824e45e813fcd5f8771e0c68d6789cc
maccyber <jonas.enge@gmail.com>
2019-09-10 19:45:54 +0200

    adds school

--- api/get-student.js
+++ api/get-student.js
@@ -1,4 +1,5 @@
 module.exports = async (request, response) => {
+  const imageUrl = 'https://robohash.org/' + Math.random() + '?set=2'
   const student = require('../test/data/student-dummy.json')
-  response.json(student)
+  response.json({ ...student, imageUrl })
 }
