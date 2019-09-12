d5b9ede5b7d41de322628e7e9b0c83be4d9abf93
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-12 07:59:40 +0200

    Implements save for send (patch)

--- api/document-send.js
+++ api/document-send.js
@@ -1,3 +1,10 @@
+const mongo = require('../lib/mongo')
+const logger = require('../lib/logger')
+
 module.exports = async (request, response) => {
-  response.send({ success: true })
+  const payload = await request.body
+  const db = await mongo()
+  const collection = db.collection(process.env.MONGODB_LOGS_COLLECTION)
+  const result = await collection.insertOne(payload)
+  response.send({ success: true, result: result })
 }
