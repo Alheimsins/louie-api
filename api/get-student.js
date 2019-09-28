f5dc1dab01af08b621a5bd346205648353c69689
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-28 07:25:01 +0200

    Adds documents dummy to student (patch)

--- api/get-student.js
+++ api/get-student.js
@@ -1,11 +1,14 @@
+const mongo = require('../lib/mongo')
+const logger = require('../lib/logger')
+const documents = require('../test/data/documents-dummy.json')
+
 module.exports = async (request, response) => {
-  const mongo = require('../lib/mongo')
-  const logger = require('../lib/logger')
   const db = await mongo()
   const collection = db.collection(process.env.MONGODB_BUDDY_COLLECTION)
   const { id } = request.query
   logger('info', ['api', 'get-student', 'id', id, 'start'])
   const students = await collection.find({ uid: id }).toArray()
   logger('info', ['api', 'get-student', 'id', id, 'get', students.length, 'students'])
-  response.json(students)
+  const results = students.map(student => Object.assign({}, student, { documents: documents }))
+  response.json(results)
 }
