7780cc4dcb3ea01acfdc5321914721928489290c
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-28 07:12:31 +0200

    Adds retreive from db to get-student (patch)

--- api/get-student.js
+++ api/get-student.js
@@ -1,5 +1,11 @@
 module.exports = async (request, response) => {
-  const student = require('../test/data/student-dummy.json')
-  const result = student.find(({ uid }) => uid === request.query.id) || {}
-  response.json(result)
+  const mongo = require('../lib/mongo')
+  const logger = require('../lib/logger')
+  const db = await mongo()
+  const collection = db.collection(process.env.MONGODB_BUDDY_COLLECTION)
+  const { id } = request.query
+  logger('info', ['api', 'get-student', 'id', id, 'start'])
+  const students = await collection.find({ uid: id }).toArray()
+  logger('info', ['api', 'get-student', 'id', id, 'get', students.length, 'students'])
+  response.json(students)
 }
