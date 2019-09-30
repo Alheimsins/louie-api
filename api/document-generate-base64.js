e354f7759dd43f03972a3518eb101bf95e8e0793
maccyber <jonas.enge@gmail.com>
2019-09-30 22:35:10 +0200

    Changes pdf creation method

--- api/document-generate-base64.js
+++ api/document-generate-base64.js
@@ -1,17 +1,16 @@
-const fetch = require('node-fetch')
+const pdfmake = require('@alheimsins/pdf-make')
 const generateTemplate = require('../lib/generate-template')
+const logger = require('../lib/logger')
 
 module.exports = async (request, response) => {
-  const { body: payload } = request
-  const template = generateTemplate(payload)
-  const options = {
-    method: 'POST',
-    body: JSON.stringify(template),
-    headers: {
-      'Content-Type': 'application/json'
-    }
+  const { body } = request
+  try {
+    const template = generateTemplate(body)
+    const base64File = (await pdfmake(template)).toString('base64')
+    response.send(`data:application/pdf;base64,${base64File}`)
+  } catch (error) {
+    logger('error', ['api', 'documents', 'generate-base64', error])
+    response.status(500)
+    response.send(error)
   }
-  const data = await fetch(process.env.PDF_SERVICE_URL, options)
-  const base64File = (await data.buffer()).toString('base64')
-  response.send(`data:application/pdf;base64,${base64File}`)
 }
--- api/document-generate.js
+++ api/document-generate.js
@@ -1,15 +1,17 @@
-const fetch = require('node-fetch')
+const pdfmake = require('@alheimsins/pdf-make')
 const generateTemplate = require('../lib/generate-template')
+const logger = require('../lib/logger')
 
 module.exports = async (request, response) => {
-  const { body: payload } = request
-  const template = generateTemplate(payload)
-  const options = {
-    method: 'POST',
-    body: JSON.stringify(template),
-    headers: { 'Content-Type': 'application/json' }
+  const { body } = request
+  try {
+    const template = generateTemplate(body)
+    const data = await pdfmake(template)
+    response.setHeader('Content-Type', 'application/pdf')
+    response.send(data.buffer())
+  } catch (error) {
+    logger('error', ['api', 'documents', 'generate', error])
+    response.status(500)
+    response.send(error)
   }
-  const data = await fetch(process.env.PDF_SERVICE_URL, options)
-  response.setHeader('Content-Type', 'application/pdf')
-  response.send(data.buffer())
 }
