398aa69c06f0c8aaab96b119506e8c08b681be39
maccyber <jonas.enge@gmail.com>
2019-10-04 15:58:01 +0200

    fix graphql query

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -1,30 +1,47 @@
-const { ApolloServer, gql } = require('apollo-server-micro')
+const { ApolloServer, ApolloError, gql } = require('apollo-server-micro')
 const getTjommiData = require('../../lib/get-tjommi-data')
+const groups = require('../../test/data/groups-dummy.json')
 
 const typeDefs = gql`
   type Student {
-    _id: String,
-    name: String,
-    uid: String,
-    username: String,
-    address: String,
-    phone: String,
-    mail: String,
-    groups: String,
-    school: String,
+    _id: ID
+    name: String
+    uid: String
+    username: String
+    address: String
+    phone: String
+    mail: String
+    groups: String
+    school: String
+    type: String
+  }
+
+  type Groups {
+    gid: String
+    sid: String
+    name: String
+    description: String
     type: String
   }
 
   type Query {
-    Students: [Student],
-    Student: Student
+    groups: [Groups],
+    students: [Student],
+    student(username: String): Student
   }
 `
-
 const resolvers = {
   Query: {
-    Students: async () => getTjommiData({ type: 'student' }),
-    Student: async (root, args, context, info) => getTjommiData({ ...args, type: 'student' })[0]
+    students: async () => getTjommiData({ type: 'student' }),
+    student: async (_, args) => {
+      try {
+        const result = await getTjommiData({ username: args.username, type: 'student' })
+        return result[0]
+      } catch (error) {
+        throw new ApolloError(error)
+      }
+    },
+    groups: () => groups
   }
 }
 
