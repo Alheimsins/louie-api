818f29340e28a668f6bd53e6d6e0c2f2de96f852
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-10-13 18:55:15 +0200

    Implements mail copy to user (minor)

--- api/document-send.js
+++ api/document-send.js
@@ -1,5 +1,7 @@
 const mongo = require('../lib/mongo')
 const validateAuth = require('../lib/auth')
+const generateMail = require('../lib/generate-mail')
+const sendMail = require('../lib/send-mail')
 // const logger = require('../lib/logger')
 
 const sendDocument = async (request, response) => {
@@ -7,6 +9,8 @@ const sendDocument = async (request, response) => {
   const db = await mongo()
   const collection = db.collection(process.env.MONGODB_LOGS_COLLECTION)
   const result = await collection.insertOne(payload)
+  const email = generateMail({ ...payload, email: request.user.email })
+  await sendMail(email)
   response.send({ success: true, result })
 }
 
